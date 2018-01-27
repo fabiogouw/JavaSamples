dedbaebb1fa51b1b641ebb10041424dc35221508
Fabio Gouw <fabiogouw@gmail.com>
2018-01-27 01:55:36 -0200

    First Spring Boot App

---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,17 @@
+package hello;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+	@RequestMapping("/")
+	public String index() {
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date date = new Date();
+		return "Greetings from Spring Boot built with Gradle: " + dateFormat.format(date); 
+	}
+}

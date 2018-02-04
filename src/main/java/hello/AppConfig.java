296db26c15f9352df66d7134172f8f0114c79e05
Fabio Gouw <fabiogouw@gmail.com>
2018-02-04 19:41:11 -0200

    receive

--- src/main/java/hello/AppConfig.java
+++ src/main/java/hello/AppConfig.java
@@ -1,18 +1,24 @@
 package hello;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.Serializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 import java.util.HashMap;
 import java.util.Map;
-
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 
 @Configuration
 public class AppConfig {
@@ -40,5 +46,29 @@ public class AppConfig {
 	@Bean
 	public KafkaTemplate<String, String> kafkaTemplate() {
 	  return new KafkaTemplate<>(producerFactory());
+	}
+
+	@Bean
+	public Map<String, Object> consumerConfigs() {
+	  Map<String, Object> props = new HashMap<>();
+	  // list of host:port pairs used for establishing the initial connections to the Kakfa cluster
+	  props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "191.235.86.51:9092");
+	  props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		props.put(ConsumerConfig.GROUP_ID_CONFIG, "transacoes");
+		props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+	  return props;
 	}	
+
+	@Bean
+	public ConsumerFactory<String, String> consumerFactory() {
+		return new DefaultKafkaConsumerFactory<>(consumerConfigs());
+	}
+
+	@Bean
+  public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>> kafkaListenerContainerFactory() {
+    ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+    factory.setConsumerFactory(consumerFactory());
+    return factory;
+  }
 }
--- src/main/java/hello/LancamentoController.java
+++ src/main/java/hello/LancamentoController.java
@@ -1,17 +1,24 @@
 package hello;
 
 import java.util.Date;
+import java.util.concurrent.CountDownLatch;
+
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class LancamentoController {
-	
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(LancamentoController.class);
+	private static int _count = 0;
+
 	@Autowired
 	private FonteDados _fonteDados;
 	@Autowired
@@ -34,5 +41,16 @@ public class LancamentoController {
 		} catch(Exception ex) {
 			throw new RuntimeException(ex);
 		}
+	}
+
+	@KafkaListener(topics = "transacoes")
+	public void receive(String payload) {
+		LOGGER.info("received payload = '{}'", payload);
+		_count++;
+	}
+	
+	@RequestMapping(value = "/kafka", method = RequestMethod.GET)
+	public int getTotal() {
+		return _count;
 	}	
 }
